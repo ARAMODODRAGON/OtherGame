d20cef7641e45c05f69e2a105540cea1f8443313
ARAMODODRAGON <domara2000@gmail.com>
2019-01-21 18:18:36 -0500

    Setting up project on github

---
+++ OtherGame/Assets/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public Vector2 movepos;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    private void Update()
+    {
+        MovePosition();
+    }
+
+    public void MovePosition()
+    {
+        if (movepos != Vector2.zero)
+        {
+            Vector2 newPos = movepos;
+
+            newPos.x += transform.position.x;
+            newPos.y += transform.position.y;
+
+            rb.MovePosition(newPos);
+        }
+    }
+}
